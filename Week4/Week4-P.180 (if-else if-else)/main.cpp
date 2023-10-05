d1603982f28e2f6a65e9718c46f4c054893526bc
Minki Shin <144317648+minki910@users.noreply.github.com>
2023-10-05 17:31:20 +0900

    Add files via upload

---
+++ Week4/Week4-P.180 (if-else if-else)/main.cpp
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int num1, num2, result;
+	char cal = 0;
+
+	printf("사칙연산 입력(정수) : ");
+	scanf("%d%c%d", &num1, &cal, &num2);
+
+	// 사칙 연산
+	if (cal == '+')
+	{
+		result = (num1 + num2);
+	}
+	else if (cal == '-')
+	{
+		result = (num1 - num2);
+	}
+	else if (cal == '/')
+	{
+		result = (num1 / num2);
+	}
+	else
+	{
+		result = (num1 * num2);
+	}
+
+	// 결과값 출력
+	printf("%d%c%d = %d\n", num1, cal, num2, result);
+
+	return 0;
+}
